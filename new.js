b52ae6a82e9d00f0fe96d955eb3f373cdfe22e0e
Palak Jain <135044747+palakjain2509@users.noreply.github.com>
2024-07-26 21:37:57 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,13 @@
+import suprsend from "@suprsend/web-sdk";
+suprsend.init(
+  c0xv95sFvrX6Tero7Ikx,
+  SS.WSS.wshQb3g5AF341IgjVmxeF7rW8sFT0hSeD_KcPqUN
+);
+suprsend.identify("pj20000925@gmail.com");
+suprsend.reset();
+
+suprsend.user.add_email("palak.22bcan249@jecrcu.edu.in"); // To add Email
+
+suprsend.user.add_sms("+918952883015"); // To add SMS
+
+suprsend.user.add_whatsapp("+918952883015"); // To add Whatsapp
